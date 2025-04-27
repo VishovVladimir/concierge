f35a94159bf0c876bc1abbc93c81036a0ac72502
v.vishov <v.vishov@unlimit.com>
2025-04-27 17:31:05 +0200

    fix

--- install.sh
+++ install.sh
@@ -18,7 +18,7 @@ echo "Installing Concierge from branch '$BRANCH_NAME'..."
 
 # === INSTALL PYTHON DEPENDENCIES ===
 sudo apt update
-sudo apt install -y python3 python3-pip
+sudo apt install -y python3 python3-pip python3.11-venv
 # === CREATE VENV ===
 python3 -m venv /home/dietpi/concierge/venv
 
